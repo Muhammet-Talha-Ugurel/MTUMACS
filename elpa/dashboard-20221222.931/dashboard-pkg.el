c1286787316278f7c0cb189981f9c0261093aa35
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2022-12-29 21:16:01 +0300

    first

---
+++ elpa/dashboard-20221222.931/dashboard-pkg.el
@@ -0,0 +1,12 @@
+(define-package "dashboard" "20221222.931" "A startup screen extracted from Spacemacs"
+  '((emacs "26.1"))
+  :commit "a69cc103aebd957f967e431399681b6d9d6b52fc" :authors
+  '(("Rakan Al-Hneiti" . "rakan.alhneiti@gmail.com"))
+  :maintainer
+  '("Jesús Martínez" . "jesusmartinez93@gmail.com")
+  :keywords
+  '("startup" "screen" "tools" "dashboard")
+  :url "https://github.com/emacs-dashboard/emacs-dashboard")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
