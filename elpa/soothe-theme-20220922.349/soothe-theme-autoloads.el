ca774736b058c9ea1784c71d07bf0afb0a837ece
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2022-12-30 07:16:28 +0300

    dt done

---
+++ elpa/soothe-theme-20220922.349/soothe-theme-autoloads.el
@@ -0,0 +1,35 @@
+;;; soothe-theme-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "soothe-theme" "soothe-theme.el" (0 0 0 0))
+;;; Generated autoloads from soothe-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "soothe-tva" "soothe-tva.el" (0 0 0 0))
+;;; Generated autoloads from soothe-tva.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(register-definition-prefixes "soothe-tva" '("soothe-tva-"))
+
+;;;***
+
+;;;### (autoloads nil nil ("soothe-theme-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; soothe-theme-autoloads.el ends here
---
+++ elpa/soothe-theme-20220922.349/soothe-theme-pkg.el
@@ -0,0 +1,11 @@
+(define-package "soothe-theme" "20220922.349" "A dark colorful theme"
+  '((emacs "24.3")
+    (autothemer "0.2"))
+  :commit "d8aee0fca549d535ebb7b5fd1a8017f12925d16b" :authors
+  '(("Jason Milkins" . "jasonm23@gmail.com"))
+  :maintainer
+  '("Jason Milkins" . "jasonm23@gmail.com")
+  :url "https://github.com/emacsfodder/emacs-soothe-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
